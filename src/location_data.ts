c9591f8c19a1706b0163d6724a272ce6fe53e8b7
Tiphereth-ver1 <long.tuanpham002@gmail.com>
2024-10-05 19:28:10 +1000

    Update location_data.ts

--- src/location_data.ts
+++ src/location_data.ts
@@ -1,5 +1,5 @@
 // location_data.ts
-
+//let message be the message that is sent
 let message: string = "23435V[1.3]B[0003266717]D[0000-00-00T00:00:00]L[0.000,0.000,600.0]I[1]R[314.953583,-11.333508,6.500096]A[-0.264000,-0.042000,0.959000]G[-10.465000,-83.466003,15.338000]IR[0,0000000000]E[TFF] @@ RD[0000-01-01T00:53:38]IL";
 
 // Function to extract data
@@ -13,7 +13,7 @@ export function extractData(message: string) {
         messageId,
         location: location?.split(',').map(Number),
         rotation: rotation?.split(',').map(Number),
-        gravity: gyroscopic_acceleration?.split(',').map(Number)
+        acceleration: gyroscopic_acceleration?.split(',').map(Number)
     };
 }
 
@@ -23,3 +23,5 @@ export function refreshData() {
     // For demonstration, we'll just return the extracted data from the existing message
     return extractData(message);
 }
+
+
